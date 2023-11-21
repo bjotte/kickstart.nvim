794930b0d0ee3536ac1f79e8a54364c880a2a980
bjotte <bjornamy@gmail.com>
2023-11-21 13:20:45 +0100

    updates during testing

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,50 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	-- File Explorer in Vim Ctrl+f
+ 	{
+ 	"nvim-telescope/telescope-file-browser.nvim",
+ 	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+ 	};
+-- Use Ctrl+fp to list recent git projects
+  	"ahmedkhalf/project.nvim",
+-- alpha dashboard
+  	{
+  	'goolord/alpha-nvim',
+  	dependencies = { 'nvim-tree/nvim-web-devicons' },
+    	config = function ()
+        require'alpha'.setup(require'alpha.themes.startify'.config)
+  	end
+  	};
+  	"jvgrootveld/telescope-zoxide",
+  	-- Colorschemes
+  	"lunarvim/darkplus.nvim",
+  	"arcticicestudio/nord-vim",
+  	"emacs-grammarly/lsp-grammarly",
+-- Quick word search under cursor alt+p and alt+n
+	"RRethy/vim-illuminate",
+-- Titus Custom
+  	"postfen/clipboard-image.nvim",
+  	"mbbill/undotree",
+  	"wakatime/vim-wakatime",
+  	"Pocco81/auto-save.nvim",
+  	"Pocco81/true-zen.nvim",
+  	"lambdalisue/suda.vim",
+  	"lunarvim/synthwave84.nvim",
+  	"github/copilot.vim",
+-- My Custom
+	"windwp/nvim-autopairs",
+	-- Optional dependency
+	dependencies = { 'hrsh7th/nvim-cmp' },
+	config = function()
+	  require("nvim-autopairs").setup {}
+	-- If you want to automatically add `(` after selecting a function or method
+	  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+	  local cmp = require('cmp')
+	  cmp.event:on(
+	  'confirm_done',
+	  cmp_autopairs.on_confirm_done()
+	  )
+	end,
+}
